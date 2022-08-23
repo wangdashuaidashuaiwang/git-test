fbfdc28b81c5c1019e12c329ecc67afefe049494
wangdashuaidashuaiwang <1162165146@qq.com>
2022-08-23 14:02:45 +0800

    third commit

--- src/main/java/com/wjt/GitTest.java
+++ src/main/java/com/wjt/GitTest.java
@@ -6,6 +6,6 @@ package com.wjt;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git1!");
     }
 }
