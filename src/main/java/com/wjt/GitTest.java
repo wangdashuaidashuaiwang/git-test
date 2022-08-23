2557935a8a91f9c15716583aac638cefd17ea546
wangdashuaidashuaiwang <1162165146@qq.com>
2022-08-23 13:57:07 +0800

    first commit

---
+++ src/main/java/com/wjt/GitTest.java
@@ -0,0 +1,11 @@
+package com.wjt;
+
+/**
+ * @author shkstart
+ * @creat 2022--23 12:10
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
