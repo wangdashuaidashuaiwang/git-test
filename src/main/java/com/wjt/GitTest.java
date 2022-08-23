80de2b48561e90109427e9152212306ed9c0bffc
wangdashuaidashuaiwang <87823484+wangdashuaidashuaiwang@users.noreply.github.com>
2022-08-23 17:23:12 +0800

    pull commit

--- src/main/java/com/wjt/GitTest.java
+++ src/main/java/com/wjt/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("master commit");
         System.out.println("hot-fix");
         System.out.println("push test");
+         System.out.println("pull test");
     }
 }
