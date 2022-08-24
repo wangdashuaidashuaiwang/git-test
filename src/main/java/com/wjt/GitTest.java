1c001898f9fb450a9335d74d131277a35c9b835e
小羊数星星 <10121612+the-lamb-counts-the-stars@user.noreply.gitee.com>
2022-08-24 05:18:19 +0000

    update src/main/java/com/wjt/GitTest.java.
    Signed-off-by: 小羊数星星 <10121612+the-lamb-counts-the-stars@user.noreply.gitee.com>

--- src/main/java/com/wjt/GitTest.java
+++ src/main/java/com/wjt/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
          System.out.println("gitee test");
          System.out.println("gitee test");
          System.out.println("gitee test");
+         System.out.println("gitee test2");
     }
 }
